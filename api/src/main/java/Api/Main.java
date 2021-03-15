421dfcb61bb9d06b2aa3893139c96b2624581beb
Nikolai Ilyushin <iljushinn@rambler.ru>
2021-03-15 18:40:02 +0300

    Maven

---
+++ api/src/main/java/Api/Main.java
@@ -0,0 +1,14 @@
+package Api;
+
+import Db.MyEntity;
+import Service.MyService;
+
+public class Main {
+
+    public static void main(String[] args) {
+        MyService myService = new MyService();
+        System.out.println(myService.getMyEntity());
+        System.out.println(myService.setMyEntity(new MyEntity("second")));
+        System.out.println(myService.getMyEntity());
+    }
+}
